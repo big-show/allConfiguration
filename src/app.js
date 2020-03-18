5cb7ce425c1a446a7f9ba066ad705770d062cfa3
big-show <626394369@qq.com>
2020-03-18 14:17:57 +0800

    des

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 /*
  * @Author: liub
  * @Date: 2019-10-11 17:13:45
- * @LastEditors  : liub
- * @LastEditTime : 2020-03-17 15:53:57
+ * @LastEditors: liub
+ * @LastEditTime: 2020-03-18 14:17:36
  */
 import React from "react"
 export default function App() {
-  const content = "ddddfsdfasdfadsfdfdfdfsdf sdfsdeedfefeedd configuration"
+  const content = "ddd sdfsdeedfefeedd configuration"
   console.log("content", content)
   return <div>{content}</div>
 }
