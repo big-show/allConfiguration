bb1cbe4f130cd17e58afcb43b873413459fcddea
big-show <626394369@qq.com>
2020-03-18 14:29:17 +0800

    sdf

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@
  * @Author: liub
  * @Date: 2019-10-11 17:13:45
  * @LastEditors: liub
- * @LastEditTime: 2020-03-18 14:19:34
+ * @LastEditTime: 2020-03-18 14:28:58
  */
 import React from "react"
 export default function App() {
-  const content = "ddd sdfsdeerrdfefeedd configuration"
+  const content = "ddd sdfsdeererererererrdfefeedd configuration"
   console.log("content", content)
   return <div>{content}</div>
 }
