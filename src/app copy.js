b998374a48e5c7f810f2fde836faa6b906523daf
big-show <626394369@qq.com>
2020-03-18 14:20:02 +0800

    se

--- src/app copy.js
+++ src/app copy.js
@@ -1,12 +1,12 @@
 /*
  * @Author: liub
  * @Date: 2019-10-11 17:13:45
- * @LastEditors  : liub
- * @LastEditTime : 2020-03-17 10:55:11
+ * @LastEditors: liub
+ * @LastEditTime: 2020-03-18 14:19:45
  */
 import React from "react"
 export default function App() {
-  const content = "hdddesdfsdfllo dddereconfiguration"
+  const content = "hddddddesdfsdfllo dddereconfiguration"
   console.log("content", content)
   return <div>{content}</div>
 }
--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@
  * @Author: liub
  * @Date: 2019-10-11 17:13:45
  * @LastEditors: liub
- * @LastEditTime: 2020-03-18 14:17:36
+ * @LastEditTime: 2020-03-18 14:19:34
  */
 import React from "react"
 export default function App() {
-  const content = "ddd sdfsdeedfefeedd configuration"
+  const content = "ddd sdfsdeerrdfefeedd configuration"
   console.log("content", content)
   return <div>{content}</div>
 }
